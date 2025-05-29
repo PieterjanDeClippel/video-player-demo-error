371bde844fdf9adc98402070d2b095e68ab9bd56
PieterjanDeClippel <pieterjandeclippel@msn.com>
2025-05-29 12:45:36 +0200

    Error demo

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,36 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter, RouterModule } from '@angular/router';
+import { streamablePlugin } from '@mintplayer/streamable-player';
+import { provideVideoApis } from '@mintplayer/ng-video-player';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { routes } from './app.routes';
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    // provideBrowserGlobalErrorListeners(),
+    // provideZoneChangeDetection({ eventCoalescing: true }),
+    // provideRouter(routes)
+    
+    importProvidersFrom(RouterModule.forRoot(routes)),
+    provideHttpClient(),
+    TranslateService,
+    provideHttpClient(withInterceptorsFromDi()),
+    provideVideoApis(streamablePlugin),
+    importProvidersFrom([
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient],
+        },
+      }),
+    ]),
   ]
 };
